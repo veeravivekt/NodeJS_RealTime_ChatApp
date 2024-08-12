293d999df3508ac04e67d2002ba86841f18e5cbe
veeravivekt <veeravivek.telagani@sjsu.edu>
2024-08-12 16:59:23 -0700

    connected to chatspage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '3b00ae13-5dbd-40c8-8e84-f49d705e38ad',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        </div>
+    )
 }
 
 export default ChatsPage
